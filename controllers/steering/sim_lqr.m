5788022ae8ea6089c757d7adda442ca0192d7540
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2023-12-20 12:27:28 +0100

    Start LQR

--- controllers/steering/sim_lqr.m
+++ controllers/steering/sim_lqr.m
@@ -6,16 +6,12 @@ controller_folder = fileparts(pwd);                     % controllers root direc
 common_sym_path = [root_folder, '/common'];             % common directory
 model_folder = [root_folder, '/models/src'];            % models directory
 road_aligned_folder = [model_folder, '/road_aligned'];  % road aligned directory
-pole_placement_folder = [pwd, '/src/pole_placement'];   % pole placement directory
-
+lqr_folder = [pwd, '/src/lqr'];                         % LQR directory
 
 % Add the new path to the MATLAB search path
 addpath(common_sym_path);           % common directory
 addpath(road_aligned_folder);       % road aligned directory
-addpath(pole_placement_folder);     % pole placement directory
-
-% Anonymous function
-pole_placement_xdot = @pole_placement_xdot;
+addpath(lqr_folder);                % LQR directory
 
 % Vehicle geomtry
 [mass, i_z, l_f, l_r, C_f, C_r] = vehicle_geometry();
@@ -43,8 +39,7 @@ end
 % Steering constraints
 [Ts, overshoot_max] = steering_constraints();
 
-% Design steering controller K
-[K, delta, S, w_n] = design_k(Ts, overshoot_max, A, B);
+% TODO: LQR
 
 % Initial state of the simulation
 x = [-2; 0; 0; 0];
@@ -66,45 +61,5 @@ y_step_response = 0;        % Step response
 while t <= t_end
     % Road Aligned integrator to calc d, psi_des(t), x_des(t) and y_des(t)
     [d, psi_des_t, x_des_t, y_des_t] = road_aligned_integrator(v_x, R, t);
-    
-    % ODE Lateral movement with controller K
-    [tsol, xsol] = ode45(@(t, x) pole_placement_xdot(x, A, B, K, B_d, d), [t t+dt], x(:,end));
-    x = [x xsol(end,:)'];     % Update the state
-    
-    % Step response
-    y = step_response(t, w_n, delta);
 
-    % Update storages
-    heading_t = x(3, end) + psi_des_t;          % PSI
-    x_posg = x_des_t - x(1,end)*sin(heading_t); % X global position
-    y_posg = y_des_t + x(1,end)*cos(heading_t); % Y global position
-    slip_t = (1/v_x)*x(2, end) - x(3, end);     % Slip angle
-    course_t = heading_t + slip_t;              % Course angle
-    u = steering_angle_u(t, freq);              % Steering angle
-
-    pos_des = [pos_des [x_des_t y_des_t]'];
-    pos_global = [pos_global [x_posg y_posg heading_t]'];
-    % heading = [heading heading_t'];
-    slip_angle = [slip_angle slip_t'];
-    course_angle = [course_angle course_t'];
-    steering_angle = [steering_angle u'];
-    y_step_response = [y_step_response y'];
-
-    % Move to the next instant (t)
-    t = t + dt;
-
-    % Change the path after 10s of the simulation
-    if t >= 10
-        % R = 1000;   % Along a circle
-    end 
 end
-
-%  Plot
-plotter(t_plot, x, pos_global, heading, slip_angle, course_angle, steering_angle, pos_des);
-
-figure
-plot(t_plot, y_step_response, 'k-', 'LineWidth', 1.5);
-title('Step response');
-xlabel('Time (s)');
-ylabel('Step response (y(t))');
-grid on;
